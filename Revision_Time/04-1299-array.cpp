cb34b7953f1f3478f9738e388730b18d1dd6eea8
Pawan Tiwari <ramtiwari7081@gmail.com>
2025-10-04 23:20:29 +0530

    adding revion on 04 oct

---
+++ Revision_Time/04-1299-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n=arr.size();
+
+        vector<int>ans(n);
+       int  g=-1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=g;
+            if(arr[i]>g||i==n-1){
+                g=arr[i];
+            }
+        }
+        
+        return ans;
+
+    }
+};
+
+//1299
